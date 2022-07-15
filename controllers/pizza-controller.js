7759dc7f09cb7f5c9d95e020bfdbea479aa0a100
Brandon <brandonperez@brandons-mbp.lan>
2022-07-15 14:00:10 -0400

    added data validation.

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -50,7 +50,7 @@ const pizzaController = {
     //update pizza by id
     // {new: true} returns the updated document, setting it to false will return original
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
